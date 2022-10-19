7a7e81ada98f18101d88c5f5bccb2de727ade8ef
maxnatalia <Nataliamazur1988@gmail.com>
2022-10-19 11:08:36 +0200

    added media queries mobile to footer

--- src/features/PersonalHomepage/Footer/styled.js
+++ src/features/PersonalHomepage/Footer/styled.js
@@ -4,6 +4,11 @@ export const Caption = styled.h2`
     text-transform: uppercase;
     font-size: 12px;
     font-weight: 700;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 12px;
+        padding: 16px;
+    }
 `;
 
 export const Address = styled.address`
@@ -16,6 +21,10 @@ export const Paragraph = styled.p`
     letter-spacing: 0.05em;
     font-weight: 400;
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 14px;
+        padding: 16px;
+    }
 `;
 
 export const Mail = styled.a`
@@ -28,21 +37,39 @@ export const Mail = styled.a`
     &:hover {
         color: ${({ theme }) => theme.elementColor.hover};
     }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 18px;
+        padding: 16px;
+    }
 `;
 
 export const LogosContainer = styled.div`
     display: flex;
     gap: 24px;
+    margin-top: 56px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        margin: 16px;
+    }
 `;
+
 export const Link = styled.a`
-
+    margin-top: 24px;
 `;
-
 
 export const styledLogo = (Logo) => styled(Logo)`
     fill: ${({ theme }) => theme.elementColor.textTitles};
-
+    transition: fill 0.3s;
+    
     &:hover {
+        & path {
         fill: ${({ theme }) => theme.elementColor.hover};
+        }
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        width: 32px;
+        height: 32px;
     }
 `;
