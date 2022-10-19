610ddbcb39dd09aa99a3c54e29c1e5615c8db6ba
maxnatalia <Nataliamazur1988@gmail.com>
2022-10-19 10:33:53 +0200

    added media queries to skills

--- src/features/PersonalHomepage/Skills/styled.js
+++ src/features/PersonalHomepage/Skills/styled.js
@@ -1,12 +1,18 @@
 import styled from "styled-components";
-import { ReactComponent as PointList } from "../../../img/point.svg";
+import { ReactComponent as PointList } from "../Skills/point.svg";
 
 export const ListWrapper = styled.section`
-    width: 1216px;
+    min-width: 1216px;
     background-color: ${({theme}) => theme.elementColor.backgroundSkills};
     padding: 32px;
     margin-top: 72px;
     box-shadow: ${({theme}) => theme.elementColor.boxShadowSkills};
+    border-radius: 4px;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+        padding: 16px;
+        min-width: 347px;
+    }
 `;
 
 export const ListHeader = styled.h2`
@@ -16,6 +22,10 @@ export const ListHeader = styled.h2`
     color: ${({ theme }) => theme.elementColor.textTitles};
     border-bottom: 1px solid ${({ theme }) => theme.elementColor.borderBottom};
     padding-bottom: 15px;
+
+    @media (max-width: ${({theme})=>theme.breakpoints.mobile}px) {
+        font-size: 18px;
+    }
 `;
 
 export const ListSkills = styled.ul`
@@ -25,6 +35,10 @@ export const ListSkills = styled.ul`
     padding: 0;
     justify-content: center;
     align-items: center;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+        grid-template-columns: 292px;
+    }
 `;
 
 export const ListItem = styled.li`
@@ -32,6 +46,10 @@ export const ListItem = styled.li`
     font-size: 18px;
     letter-spacing: 0.05em;
     font-weight: 400;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+        font-size: 14px;
+    }
 `;
 
 export const Point = styled(PointList)`
