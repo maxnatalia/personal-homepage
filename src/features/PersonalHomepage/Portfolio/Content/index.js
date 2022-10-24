37c93bde64137f73042d579c908d368a133bf99c
maxnatalia <Nataliamazur1988@gmail.com>
2022-10-24 06:45:48 +0200

    added new component content

---
+++ src/features/PersonalHomepage/Portfolio/Content/index.js
@@ -0,0 +1,24 @@
+import Loading from "./Loading";
+import Error from "./Error";
+import Repositories from "./Repositories";
+
+const Content = ({ status, repositories }) => {
+    switch (status) {
+        case "initial":
+            return null;
+
+        case "loading":
+            return <Loading />;
+
+        case "error":
+            return <Error />;
+
+        case "success":
+            return <Repositories repositories={repositories} />;
+
+        default:
+            throw new Error(`incorrect status: ${status}`);
+    }
+};
+
+export default Content;
