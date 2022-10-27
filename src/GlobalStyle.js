650accdca8a0e3e4eb803347c1bdb2b16e13a759
maxnatalia <Nataliamazur1988@gmail.com>
2022-10-27 07:39:54 +0200

    changed global style - added transition

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -10,12 +10,15 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background-color: ${({theme}) => theme.elementColor.background};
-        color: ${({theme}) => theme.elementColor.text};
+        background-color: ${({theme}) => theme.background.body};
+        color: ${({theme}) => theme.text};
+        transition: background 0.3s;
         letter-spacing: 0.05em;
         line-height: 1.4;
         font-family: 'Inter', sans-serif;
         font-weight: 400;
-        margin: 0;
+        margin: 0 auto;
+        max-width: 1216px;
+        word-break: break-word;
     }
 `;
