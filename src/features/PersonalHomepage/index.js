61c08d74d4b85a5df68b0511dd371606874be72a
maxnatalia <Nataliamazur1988@gmail.com>
2022-10-27 07:41:30 +0200

    updated personal homepage according to skillsData

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -1,22 +1,25 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Skills from "./Skills";
-import { skillsList, toLearnSkillsList } from "./Skills/skillsData";
-import { Main } from "./Main";
+import Portfolio from "./Portfolio";
+import { skillsData } from "./Skills/skillsData";
+import { Main } from "./styled";
+import { nanoid } from "@reduxjs/toolkit";
 
 const PersonalHomepage = () => {
     return (
         <>
             <Header />
             <Main>
-                <Skills
-                    title={"My skillset includes"}
-                    list={skillsList}
-                />
-                <Skills
-                    title={"What I want to learn next"}
-                    list={toLearnSkillsList}
-                />
+                {skillsData.map((skillsInfo) => (
+                    <Skills
+                        key={nanoid()}
+                        title={skillsInfo.title}
+                        icon={skillsInfo.icon}
+                        list={skillsInfo.list}
+                    />
+                ))}
+                <Portfolio />
             </Main>
             <Footer />
         </>
